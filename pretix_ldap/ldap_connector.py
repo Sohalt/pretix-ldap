a3dc4e25b750b05a33ff8c2363f72b50345486aa
sohalt <sohalt@sohalt.net>
2020-06-05 13:02:35 +0200

    More logging

--- pretix_ldap/ldap_connector.py
+++ pretix_ldap/ldap_connector.py
@@ -19,8 +19,11 @@ class LDAPAuthBackend(BaseAuthBackend):
             self.connection = Connection(self.server, self.config['bind_dn'], self.config['bind_password'], auto_bind=True)
             self.search_base = self.config['search_base']
         except KeyError:
-            logger.error("please specify bind_url, bind_dn, bind_password, and search_base in [ldap] section in pretix.cfg")
+            logger.error("Please specify bind_url, bind_dn, bind_password, and search_base in [ldap] section in pretix.cfg")
         self.search_filter_template = self.config.get('search_filter', fallback='(&(objectClass=inetOrgPerson)(mail={email}))')
+        self.placeholders = re.findall('{([^{}]+)}', self.search_filter_template)
+        if self.placeholders == {}:
+            logger.error("Please specify at least one placeholder in your search_filter")
         self.email_attr = self.config.get('email_attr', fallback='mail')
 
     @property
@@ -34,8 +37,7 @@ class LDAPAuthBackend(BaseAuthBackend):
     @property
     def login_form_fields(self):
         # create text fields for all placeholders in the search string
-        placeholders = re.findall('{([^{}]+)}', self.search_filter_template)
-        fields = {p: forms.CharField(label=p) for p in placeholders}
+        fields = {p: forms.CharField(label=p) for p in self.placeholders}
         fields['password'] = forms.CharField(label=_('Password'), widget=forms.PasswordInput)
         # automatically focus the first field
         list(fields.values())[0].widget.attrs['autofocus'] = 'autofocus'
@@ -44,8 +46,7 @@ class LDAPAuthBackend(BaseAuthBackend):
     def form_authenticate(self, request, form_data):
         from pretix.base.models import User
         password = form_data['password']
-        placeholders = re.findall('{([^{}]+)}', self.search_filter_template)
-        template_data = {p: escape_filter_chars(form_data[p]) for p in placeholders}
+        template_data = {p: escape_filter_chars(form_data[p]) for p in self.placeholders}
         filter = self.search_filter_template.format_map(template_data)
         if not self.connection.search(self.search_base, filter, attributes=[self.email_attr]):
             # user not found
@@ -53,11 +54,13 @@ class LDAPAuthBackend(BaseAuthBackend):
         res = self.connection.response
         if len(res) != 1:
             # could not uniquely identify user
+            logger.warn("Could not uniquely identify user. Check your search_filter")
             return None
         dn = res[0]['dn']
         emails = res[0]['attributes'][self.email_attr]
         if len(emails) != 1:
             # could not uniquely identify user email
+            logger.warn("Could not uniquely identify user email")
             return None
         email = emails[0]
         try:
