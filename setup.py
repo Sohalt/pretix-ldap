dd17ee69d7337e4b7d8b936d37caf9533cf1c9a6
sohalt <sohalt@sohalt.net>
2024-04-15 14:20:21 +0200

    Version 0.2.5

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ except:  # NOQA
 
 setup(
     name="pretix-ldap",
-    version="0.2.4",
+    version="0.2.5",
     description="LDAP authentication backend for pretix",
     long_description=long_description,
     url="https://github.com/Sohalt/pretix-ldap",
